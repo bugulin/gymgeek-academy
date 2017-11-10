d1faa6c2c2d0858912be96e7300eebc3d8d1afc1
bugulin <petriczech12@gmail.com>
2017-11-10 13:40:55 +0100

    Materiály pro další hodinu - prvočísla.

--- cyklus_for.py
+++
@@ -1,4 +0,0 @@
-seznam = [1, 3, 5, 3, "ahoj"]
-
-for i in range(len(seznam)):
-    print(seznam[i])
--- geometrie.py
+++
@@ -1,7 +0,0 @@
-def obsah(a, b):
-    S = a * b
-    print("Obsah obdélníku o stranách", a, "a", b, "je", S)
-    
-def pozdrav(n):
-    for i in range(n):
-        print("Nazdar", i)
